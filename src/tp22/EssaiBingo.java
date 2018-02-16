4f445d10b5b2d80ec4e4a87c416494f1d8dbbdac
You Nous <younss@mailoo.org>
2018-02-16 13:06:55 +0100

    update code in main EssaiBingo class

--- src/tp22/EssaiBingo.java
+++ src/tp22/EssaiBingo.java
@@ -7,31 +7,25 @@ public class EssaiBingo {
 	public static void main(String[] args) {
 	    Random r = new Random();
 
-		Joueur alice = new Joueur("Alice");
-		Joueur bob = new Joueur("Bob");
-		Joueur chloe = new Joueur("Chloé");
+	    Joueur[] joueurs = new Joueur[]{
+	            new Joueur("Alice"),
+                new Joueur("Bob"),
+                new Joueur("Chloé")
+	    };
+        for (Joueur joueur: joueurs) {
+            joueur.affiche();System.out.println();
+        }
 
-		alice.affiche();
-		System.out.println();
-		bob.affiche();
-		System.out.println();
-		chloe.affiche();
-		System.out.println();
-
-		do{
-		    int n = r.nextInt(99)+1;
-		    if(alice.coup(n) && alice.gagnant()){
-                System.out.println("Alice a gagné");
-                break;
+        boolean isWinner = false;
+		do {
+            int n = r.nextInt(99) + 1;
+            for (Joueur joueur : joueurs) {
+                if (joueur.coup(n) && joueur.gagnant()) {
+                    System.out.println(joueur.nom + " a gagné");
+                    isWinner = true;
+                    break;
+                }
             }
-		    if(bob.coup(n) && bob.gagnant()){
-                System.out.println("Bob a gagné");
-                break;
-            }
-		    if(chloe.coup(n) && chloe.gagnant()){
-                System.out.println("Chloé a gagné");
-                break;
-            }
-        }while (true);
+        }while(!isWinner);
 	}
 }

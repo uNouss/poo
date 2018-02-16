9220a5d215a03cb10879a8f6b734bf3b8ea5ccaf
You Nous <younss@mailoo.org>
2018-02-17 00:15:47 +0100

    update EssaiGrille

--- src/tp21/EssaiGrille.java
+++ src/tp21/EssaiGrille.java
@@ -5,11 +5,11 @@ public class EssaiGrille {
 	public static void main(String[] args) {
 		Grille grid = new Grille(3, 5);
 		
-		//grid.remplir(0, 200);
-		//grid.affiche();
-		//grid.fenetre(0, 0);
+		grid.remplir(0, 200);
+		grid.affiche();
+		grid.fenetre(0, 0);
 		
-		grid.remplir(1, 99, false, 0.5);
+		grid.remplir(1, 99, false, 1);
 		grid.affiche();
 	}
 

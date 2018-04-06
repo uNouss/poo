a44566ea28bf921ad579974cc115f926c393ce78
You Nous <younss@mailoo.org>
2018-04-06 17:10:11 +0200

    debut coo tp 9

--- src/coo/tp9/OrdoPseudoEquitable.java
+++ src/coo/tp9/OrdoPseudoEquitable.java
@@ -10,11 +10,22 @@ public class OrdoPseudoEquitable extends OrdoAvecPriorite{
 
     @Override
     public Prioritaire plusPrioritaire() {
+        int priority;
         for(int i = 0; i < priorite.length; i++) {
             if(priorite[i] > i+1) {
+                priorite[i] = 0;
                 return ordofile[i].plusPrioritaire();
             }
         }
+        Prioritaire p = super.plusPrioritaire();
+        priority = p.getPriorite();
+        changePriorite(priority);
         return super.plusPrioritaire();
+    }
+
+    private void changePriorite(int priority) {
+        for(int i = 0; i < priorite.length; i++){
+            if(i != priority) priorite[i] += 1;
+        }
     }
 }
--- src/coo/tp9/Systeme.java
+++ src/coo/tp9/Systeme.java
@@ -2,7 +2,7 @@ package coo.tp9;
 
 public class Systeme {
     public static void main(String[] args) {
-        OrdoAvecPriorite oap = new OrdoAvecPriorite(12);
+        Ordonnanceur oap = new OrdoAvecPriorite(12);
 
         oap.ajouteElt(new ProcessusAfficheur("j'affiche"));
         oap.ajouteElt(new ProcessusBoucleur("je boucle", 5));
@@ -12,11 +12,27 @@ public class Systeme {
         oap.ajouteElt(new ProcessusAfficheur("j'affiche"));
 
 
-        while(!oap.estVide()){
-            Processus p = (Processus) oap.plusPrioritaire();
-            p.execute();
-            if(!p.estFini()) oap.ajouteElt((Prioritaire)p);
-        }
+        runProcessus(oap);
 
+        Ordonnanceur oap1 = new OrdoPseudoEquitable(12);
+
+        oap1.ajouteElt(new ProcessusAfficheur("j'affiche"));
+        oap1.ajouteElt(new ProcessusBoucleur("je boucle", 5));
+        oap1.ajouteElt(new ProcessusLectureEcriture());
+        oap1.ajouteElt(new ProcessusBoucleur("je boucle aussi", 4));
+        oap1.ajouteElt(new ProcessusBoucleur("je boucle encore", 6));
+        oap1.ajouteElt(new ProcessusAfficheur("j'affiche"));
+
+
+        runProcessus(oap1);
+
+    }
+
+    private static void runProcessus(Ordonnanceur oap1) {
+        while(!oap1.estVide()){
+            Processus p = (Processus) oap1.plusPrioritaire();
+            p.execute();
+            if(!p.estFini()) oap1.ajouteElt((Prioritaire)p);
+        }
     }
 }

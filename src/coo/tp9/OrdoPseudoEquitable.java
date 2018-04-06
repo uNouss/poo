c875c4416742a03ddaa2262ca035a10059270014
You Nous <younss@mailoo.org>
2018-04-06 16:52:48 +0200

    debut coo tp 9

--- src/coo/tp9/OrdoPseudoEquitable.java
+++ src/coo/tp9/OrdoPseudoEquitable.java
@@ -8,5 +8,18 @@ public class OrdoPseudoEquitable extends OrdoAvecPriorite{
         this.priorite = new int[taille];
     }
 
+    @Override
+    public Prioritaire plusPrioritaire() {
+        Prioritaire p = super.plusPrioritaire();
+        int priority = p.getPriorite();
+        for(int i = 0; i < priorite.length; i++) {
+            priorite[i] += 1;
+        }
+        priorite[priority] = 0;
+        ((Processus)p).execute();
+        return null;
+    }
+
+
 
 }
